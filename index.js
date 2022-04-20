9ffb28527ed683fd3b1249918e654b098314f07b
Chandra <Chandrabachtiar25@gmail.com>
2022-04-20 14:42:05 +0700

    add fungsi sticker

--- index.js
+++ index.js
@@ -1,5 +1,7 @@
+import chalk from 'chalk';
 import wa from 'whatsapp-web.js';
-const { Client, LocalAuth } = wa;
+const { Client, LocalAuth, Chat } = wa;
+const log = console.log;
 
 const client = new Client({
     authStrategy: new LocalAuth(),
@@ -8,12 +10,29 @@ const client = new Client({
     },
 });
 
+const chat = new Chat();
+
+client.on('message', async (msg) => {
+    if (msg.body.includes("!sticker")) {
+        if (msg.hasMedia) {
+            const media = await msg.downloadMedia();
+            client.sendMessage(msg.from, media, {
+                sendMediaAsSticker: true,
+                stickerName: `[BOT STICKER]`,
+                stickerAuthor: "CBot!",
+            });
+        } else {
+            client.sendMessage(msg.from, "Foto tidak ditemukan!");
+        }
+    }
+});
+
 client.on('qr', (qr) => {
     console.log('QR RECEIVED', qr);
 });
 
 client.on('ready', () => {
-    console.log('Client is ready!');
+    log(chalk.green('[SERVER] WA WEB Service ready'));
 });
 
 client.initialize();
