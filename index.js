93dd2b8b35d320eaecf4813e3b4cdb9cbe623ed0
Chandra <Chandrabachtiar25@gmail.com>
2022-04-20 10:55:46 +0700

    Start WA

--- index.js
+++ index.js
@@ -1,1 +1,19 @@
-console.log('ok');
+import wa from 'whatsapp-web.js';
+const { Client, LocalAuth } = wa;
+
+const client = new Client({
+    authStrategy: new LocalAuth(),
+    puppeteer: {
+        headless: false,
+    },
+});
+
+client.on('qr', (qr) => {
+    console.log('QR RECEIVED', qr);
+});
+
+client.on('ready', () => {
+    console.log('Client is ready!');
+});
+
+client.initialize();
